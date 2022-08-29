21976a504243c07eb572506ff4996415e8c5c853
Sunisa Thisansang <sunisa.pampam@gmail.com>
2022-08-29 21:03:53 +0700

    gfcvgyv

--- user/index.php
+++ user/index.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Document</title>
     <link rel="stylesheet" href="../css/indax.css">
 
-    <?php include "../fuction/importlink.php" ?>
-    <?php include "nav.php" ?>
+    <?php
+    include "./nav.php";
+    include "./fuction/connectDB.php";
+    ?>
 </head>
+
+
 <body>
-    
+  
 </body>
+
 </html>
