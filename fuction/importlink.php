c4fa993438a1d322ece7d05290ad5492765f7a5a
srlparty <party080343@gmail.com>
2022-08-29 17:49:56 +0700

    ggg

--- fuction/importlink.php
+++ fuction/importlink.php
@@ -12,6 +12,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <!-- icon -->
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap');
+  </style>
     <title>link</title>
 </head>
 
--- indax.php
+++ indax.php
@@ -7,13 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/indax.css">
     <title>Index</title>
-    <?php include "./nav.php" ?>
+    <?php
+    include "./nav.php";
+    include "./fuction/connectDB.php";
+    ?>
 </head>
 
 <body>
 
     <img class="image-hotel" src="./img/banner1.png" class="d-block w-100" alt="...">
 
+    <!-- container Search -->
     <div class="container text-center">
         <div class="row">
             <div class="col-md-3 ">
@@ -44,9 +48,55 @@
         </div>
     </div>
 
-    <p>party 555</p>
+    <!-- card public relations -->
+    <h2>ประชาสัมพันธ์</h2>
+    <div class="card-body news">
+        ที่พักบรรยากาศอบอุ่น บ้านสวน ริมคลองท้องทุ่งนา บรรยากาศแห่งการพักผ่อน ที่ห้อมล้อมด้วยธรรมชาติ บรรยากาศแห่งความสุข และผ่อนคลาย ที่ "วิลล่าเดอพันตา" ภูเรือ
+    </div>
 
-    <p>mink</p>
+
+
+    <!-- card room-->
+    <h3>ห้องพัก</h3>
+    <div class="card rd mb-3 ">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="./img/bgvilla.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body room-detail">
+                    <h3 class="title">Card title</h3>
+                    <p class="card-t">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-t"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <button type="submit" class="subroom">ดูรายละเอียดเพิ่มเติม</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- map -->
+    <div class="Villa">
+    <h3>สถาที่ใกล้เคียง</h3>
+    <div class="mapvilla">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.0649722229796!2d101.37282671467175!3d17.456604088037206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3121273d1cd2b60d%3A0x71bd6196d0c3b524!2z4LmC4Lij4LiH4LmB4Lij4Lih4Lin4Li04Lil4Lil4LmI4Liy4LmA4LiU4Lit4Lie4Lix4LiZ4LiV4Liy!5e0!3m2!1sth!2sth!4v1661761437813!5m2!1sth!2sth"  width="750" height="450"  style="border-radius: 10px;"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="nearbyplaces">
+            <ul>ภูเรือ - 1.3 กม.</ul>
+            <ul>โรงพยาบาลภูเรือ - 2.3 กม.</ul>
+            <ul>จุดชมวิวเดโช - 3.6 กม.</ul>
+            <ul>อุทยานแห่งชาติภูเรือ - 4.9 กม.</ul>
+            <ul>หน้าผาสามทอง - 5.2 กม.</ul>
+            <ul>ภูเรือแคมป์ไซต์ - 6.2 กม.</ul>
+            <ul>ผาโหล่นน้อย - 6.4 กม.</ul>
+            <ul>น้ําตกปลาบ่า - 7.2 กม.</ul>
+            <ul>ภูเรือ ใจดี กอล์ฟ - 15.2 กม.</ul>
+        </div>
+    </div>
+</div>
+    <!-- About the hotel -->
+    <div class="cabouthotel">
+  <img src="./img/bbg.png" class="card-img" alt="...">
+
+</div>
 
 </body>
 
