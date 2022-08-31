45f530c307c2c5e6b8876b621714e904bf5e9c3d
Josemink <112331800+Josemink@users.noreply.github.com>
2022-08-31 15:18:21 +0700

    hohho

--- admin/manage.php
+++ admin/manage.php
@@ -2,59 +2,61 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>board</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>board</title>
 
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
 
-  <link rel="stylesheet" href="../css/manage.css">
-  <?php include "../admin/nav-admin.php";
-  include "../fuction/importlink.php" ?>
+    <link rel="stylesheet" href="../css/manage.css">
+    <?php include "../admin/nav-admin.php";
+    include "../fuction/importlink.php" ?>
 </head>
 
 <body>
-<h3>จัดการห้องพัก</h3>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary addroom style = color:#EF774B" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  เพิ่มห้องพัก
-</button>
+    <h3>จัดการห้องพัก</h3>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary addroom" style="background-color: #EF774B; border:#EF774B;" data-bs-toggle="modal" data-bs-target="#addroom">
+        เพิ่มห้องพัก
+    </button>
 
-  <table id="myTable" class="display" style="width: 100%;">
-    <thead>
-      <tr>
-        <th>เลขห้อง</th>
-        <th>ประเภทห้อง</th>
-        <th>ประเภทเตียง</th>
-        <th>สิ่งอำนวยความสะดวก</th>
-        <th>#</th>
-        <th>ราคา</th>
-        <th>จัดการ</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>..</td>
-        <td>..</td>
-        <td>..</td>
-        <td>..</td>
-        <td>..</td>
-        <td>...</td>
-        <td>
-            <div class="edit-delete">
-
-            </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  <?php require '../fuction/modal-addroom.php' ?>
-  <script>
-    $(document).ready(function() {
-      $("#myTable").DataTable();
-    });
-  </script>
+    <table id="myTable" class="display" style="width: 100%;">
+        <thead>
+            <tr>
+                <th>เลขห้อง</th>
+                <th>ประเภทห้อง</th>
+                <th>ประเภทเตียง</th>
+                <th>สิ่งอำนวยความสะดวก</th>
+                <th>จำนวนผู้เข้าพัก</th>
+                <th>ราคา</th>
+                <th>จัดการ</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>..</td>
+                <td>..</td>
+                <td>..</td>
+                <td>..</td>
+                <td>..</td>
+                <td>...</td>
+                <td>
+                    <div class="edit-delete">
+                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editroom"><i class='bx bx-edit-alt'></i>แก้ไข</button>
+                        <button type="button" class="btn btn-danger"><i class='bx bx-trash' ></i>ลบ</button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <?php require '../fuction/modal-addroom.php' ;
+    require '../fuction/modal-editroom.php'?>
+    <script>
+        $(document).ready(function() {
+            $("#myTable").DataTable();
+        });
+    </script>
 </body>
 
 </html>
