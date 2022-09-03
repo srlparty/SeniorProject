f4fd26eaf445708bfc9d401f491b4ddf267c45b9
Josemink <112331800+Josemink@users.noreply.github.com>
2022-09-03 20:25:11 +0700

    ้่่เfffggg

--- fuction/modal-user.php
+++ fuction/modal-user.php
@@ -1,17 +1,66 @@
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="staticBackdropLabel">ดูรายละเอียดเพิ่มเติม</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">รายละเอียดเพิ่มเติม</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- รูปห้อง -->
+                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                                 </div>
-                                <div class="modal-body">
-                               ...
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="../img/banner1.png" class="d-block w-100" alt="...">
+                                    </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
-                                    <button type="button" class="btn btn-primary" >จอง</button>
-                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
                             </div>
                         </div>
+                        <h6 class="typeroom"><b>ใส่โค้ดประเภทห้อง</b></h6><br>
+                        <div class="col-md-4">
+                            <p class="room"><b>ห้องนอน</b></p>
+                            <p class="room">ใส่โค้ดเตียง</p>
+                            <p class="room">ใส่ห้องน้ำ</p><br>
+
+                            <p class="room"><b>ห้องนอน</b></p>
+                            <p class="room">ห้องปลอดภัย</p><br>
+                            <p class="room">มีพื้นที่นั่งเล่น</p><br>
+                        </div>
+                        <div class="col-md-8 ms-auto">
+                            <p class="room"><b>สิ่งอำนวยความสะดวก</b></p>
+                            <p class="room">ใส่โค้ดสิ่งอำนวยความสะดวก</p>
+                        </div>
+                        <div class="col-md-4 ms-auto">
+                            <p class="room"><b>ทิวทัศน์บริเวณที่พัก</b></p>
+                            <p class="room">ภูเขา</p>
+                            <p class="room">ทุ่งนา</p><br>
+
+                            <p class="room"><b>พื้นที่ส่วนรวม</b></p>
+                            <p class="room">ร้านกาแฟ</p>
+                            <p class="room">ร้านอาหาร</p>
+                            <p class="room">คาเฟ่ทุ่งนา</p>
+                            <p class="room">ที่จอดรถ</p>
+                        </div>
                     </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" style="background-color: #EF774B; border : #EF774B;">จอง</button>
+                </div>
+            </div>
+        </div>
+    </div>
--- user/index-user.php
+++ user/index-user.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../css/indax.css">
 
     <?php
-    
+
     include "./nav.php";
     include "../fuction/connectDB.php";
     ?>
@@ -24,99 +24,110 @@
 
     <!-- container Search -->
 
-<body>
+    <body>
 
-    <div class="container text-center">
-        <div class="row">
-        <div class="col-md-3 ">
-                <p>วันที่เช็คอิน</p>
-                <form method="POST" class="hstack gap-4 mt-4">
-                    <label>เลือกวันที่เช็คอิน</label>
-                    <input type="date" class="form-control" style="width: 50%;">
-                </form>
-            </div>
-            <div class="col-md-3" style="border-left: 1px solid rgb(221, 221, 221) ;">
-                <p> วันที่เช็คเอ้าท์</p>
-                <form method="POST" class="hstack gap-4 mt-4">
-                    <label>เลือกวันที่เช็คเอ้าท์</label>
-                    <input type="date" class="form-control" style="width: 50%;">
-                </form>
-            </div>
-            <div class="col-md-3" style="border-left: 1px solid rgb(221, 221, 221) ;">
-                <p>จำนวนคน</p>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-3 ">
+                    <p>วันที่เช็คอิน</p>
                     <form method="POST" class="hstack gap-4 mt-4">
-                <label class="form-check-label" for="flexCheckDefault">ผู้ใหญ่</label>
-                <input id="number" type="number" value="" class="form-control" id="bed1" name="1" aria-describedby="emailHelp">
-                <label class="form-check-label" for="flexCheckDefault">เด็ก</label>
-                <input id="number" type="number" value="" class="form-control" id="bed1" name="1" aria-describedby="emailHelp">
-                </form>
+                        <label>เลือกวันที่เช็คอิน</label>
+                        <input type="date" class="form-control" style="width: 50%;">
+                    </form>
+                </div>
+                <div class="col-md-3" style="border-left: 1px solid rgb(221, 221, 221) ;">
+                    <p> วันที่เช็คเอ้าท์</p>
+                    <form method="POST" class="hstack gap-4 mt-4">
+                        <label>เลือกวันที่เช็คเอ้าท์</label>
+                        <input type="date" class="form-control" style="width: 50%;">
+                    </form>
+                </div>
+                <div class="col-md-3" style="border-left: 1px solid rgb(221, 221, 221) ;">
+                    <p>จำนวนคน</p>
+                    <form method="POST" class="hstack gap-4 mt-4">
+                        <label class="form-check-label" for="flexCheckDefault">ผู้ใหญ่</label>
+                        <input id="number" type="number" value="" class="form-control" id="bed1" name="1" aria-describedby="emailHelp">
+                        <label class="form-check-label" for="flexCheckDefault">เด็ก</label>
+                        <input id="number" type="number" value="" class="form-control" id="bed1" name="1" aria-describedby="emailHelp">
+                    </form>
+                </div>
+                <button class="col-md-3 btnsearch" href="">
+                    ค้นหา
+                </button>
             </div>
-            <button class="col-md-3 btnsearch" href="">
-                ค้นหา
-            </button>
         </div>
-    </div>
 
 
-    <!-- card public relations -->
-    <h2>ประชาสัมพันธ์</h2>
-    <div class="card-body news">
-        ที่พักบรรยากาศอบอุ่น บ้านสวน ริมคลองท้องทุ่งนา บรรยากาศแห่งการพักผ่อน ที่ห้อมล้อมด้วยธรรมชาติ บรรยากาศแห่งความสุข และผ่อนคลาย ที่ "วิลล่าเดอพันตา" ภูเรือ
-    </div>
+        <!-- card public relations -->
+        <h2>ประชาสัมพันธ์</h2>
+        <div class="card-body news">
+            ที่พักบรรยากาศอบอุ่น บ้านสวน ริมคลองท้องทุ่งนา บรรยากาศแห่งการพักผ่อน ที่ห้อมล้อมด้วยธรรมชาติ บรรยากาศแห่งความสุข และผ่อนคลาย ที่ "วิลล่าเดอพันตา" ภูเรือ
+        </div>
 
 
 
-    <!-- card room-->
-    <h3>ห้องพัก</h3>
-    <div class="card rd mb-3 ">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="../img/bgvilla.png" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body room-detail">
-                    <h3 class="title">Card title</h3>
-                    <p class="card-t">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-t"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    <!-- Button trigger modal -->
-                    <button type="button" class="subroom " data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        ดูรายละเอียดเพิ่มเติม
-                    </button>
+        <!-- card room-->
+        <h3>ห้องพัก</h3>
+        <div class="card rd mb-3 ">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../img/bgvilla.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body room-detail" style="height: 350px;">
+                        <h3 class="title"><b>ใส่โค้ดประเภทห้อง เตียง</b></h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="card-t"><i class='bx bx-wifi'></i>ฟรี Wi-Fi</p>
+                                <p class="card-t"></i><i class='bx bx-x'></i>ห้องปลอดบุหรี่</p>
+                                <p class="card-t"><i class='bx bxs-bowl-rice' ></i>ไม่รวมอาหารเช้า</p>
+                                <p class="card-t"><i class='bx bxs-dog' ></i>ไม่อนุญาตให้นำสัตว์เลี้ยงเข้าห้องพัก</p>
 
-             
 
+                                <button type="button" class="btn btn-link" style="color: #EF774B;" data-bs-toggle="modal" data-bs-target="#staticBackdrop">รายละเอียดเพิ่มเติม</button>
+
+                            </div>
+                            <div class="col-md-3 ms-auto">
+                                <p class="card-t"><b>ราคา</b></p>
+                                <p class="card-t"><h1>ใส่โค้ดราคา</h1></p>
+                            </div>
+                        </div>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="subroom ">
+                            จอง
+                        </button>
+                    </div>
+                </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <!-- map -->
-    <div class="Villa">
-        <h3>สถานที่ใกล้เคียง</h3>
-        <div class="mapvilla">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.0649722229796!2d101.37282671467175!3d17.456604088037206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3121273d1cd2b60d%3A0x71bd6196d0c3b524!2z4LmC4Lij4LiH4LmB4Lij4Lih4Lin4Li04Lil4Lil4LmI4Liy4LmA4LiU4Lit4Lie4Lix4LiZ4LiV4Liy!5e0!3m2!1sth!2sth!4v1661761437813!5m2!1sth!2sth" width="750" height="450" style="border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <div class="nearbyplaces">
-                <ul>ภูเรือ - 1.3 กม.</ul>
-                <ul>โรงพยาบาลภูเรือ - 2.3 กม.</ul>
-                <ul>จุดชมวิวเดโช - 3.6 กม.</ul>
-                <ul>อุทยานแห่งชาติภูเรือ - 4.9 กม.</ul>
-                <ul>หน้าผาสามทอง - 5.2 กม.</ul>
-                <ul>ภูเรือแคมป์ไซต์ - 6.2 กม.</ul>
-                <ul>ผาโหล่นน้อย - 6.4 กม.</ul>
-                <ul>น้ําตกปลาบ่า - 7.2 กม.</ul>
-                <ul>ภูเรือ ใจดี กอล์ฟ - 15.2 กม.</ul>
+            <!-- map -->
+            <div class="Villa">
+                <h3>สถานที่ใกล้เคียง</h3>
+                <div class="mapvilla">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.0649722229796!2d101.37282671467175!3d17.456604088037206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3121273d1cd2b60d%3A0x71bd6196d0c3b524!2z4LmC4Lij4LiH4LmB4Lij4Lih4Lin4Li04Lil4Lil4LmI4Liy4LmA4LiU4Lit4Lie4Lix4LiZ4LiV4Liy!5e0!3m2!1sth!2sth!4v1661761437813!5m2!1sth!2sth" width="750" height="450" style="border-radius: 10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="nearbyplaces">
+                        <ul>ภูเรือ - 1.3 กม.</ul>
+                        <ul>โรงพยาบาลภูเรือ - 2.3 กม.</ul>
+                        <ul>จุดชมวิวเดโช - 3.6 กม.</ul>
+                        <ul>อุทยานแห่งชาติภูเรือ - 4.9 กม.</ul>
+                        <ul>หน้าผาสามทอง - 5.2 กม.</ul>
+                        <ul>ภูเรือแคมป์ไซต์ - 6.2 กม.</ul>
+                        <ul>ผาโหล่นน้อย - 6.4 กม.</ul>
+                        <ul>น้ําตกปลาบ่า - 7.2 กม.</ul>
+                        <ul>ภูเรือ ใจดี กอล์ฟ - 15.2 กม.</ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <!-- About the hotel -->
-    <div class="cabouthotel">
-        <img src="../img/bbg.png" class="card-img" alt="...">
+            <!-- About the hotel -->
+            <div class="cabouthotel">
+                <img src="../img/bbg.png" class="card-img" alt="...">
 
-    </div>
+            </div>
 
-    <?php  require '../fuction/modal-user.php'?>
+            <?php require '../fuction/modal-user.php' ?>
 
 
-</body>
+    </body>
 
 </html>
