90105ba2a897948313219e55df1a8b06a6ea9780
jeonmink <minkmink0092@gmail.com>
2022-11-05 19:57:42 +0700

    aa

--- admin/nav-admin.php
+++ admin/nav-admin.php
@@ -50,7 +50,7 @@
      </li>
       <li>
        <a href="./categories.php">
-       <i class='bx bxs-bed'></i> 
+       <i class='bx bx-list-ul'></i>
          <span class="links_name">จัดการประเภทห้องพัก</span>
        </a>
        <span class="tooltip">จัดการประเภทห้องพัก</span>
@@ -103,6 +103,13 @@
          <span class="links_name">บัญชีผู้ใช้</span>
        </a>
        <span class="tooltip">บัญชีผู้ใช้</span>
+     </li>
+     <li>
+       <a href="./setting.php">
+       <i class='bx bx-cog'></i>
+         <span class="links_name">เกี่ยวกับฉัน</span>
+       </a>
+       <span class="tooltip">เกี่ยวกับฉัน</span>
      </li>
      <li class="profile">
          <div class="profile-details">
